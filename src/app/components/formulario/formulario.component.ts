401e9b265273429a28f50925c662d21d0d2d1e58
brunonovell <brunosantoscomercial@hotmail.com>
2021-12-06 16:50:19 -0300

    Mapeamento de id

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatListItem } from '@angular/material/list';
 
 @Component({
   selector: 'app-formulario',
@@ -10,6 +11,11 @@ export class FormularioComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  pesquisar(): void{
+
   }
 
 }
